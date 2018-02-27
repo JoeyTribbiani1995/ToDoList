9b93dbe809d26d82d4b5a1e8d512f628b34afaa1
JoeyTribbiani <vophanhongdung@gmail.com>
2018-02-28 03:56:34 +0700

    second commit display models

---
+++ ToDoList/Models/ToDoLIst.swift
@@ -0,0 +1,29 @@
+//
+//  ToDoLIst.swift
+//  ToDoList
+//
+//  Created by Dũng Võ on 2/28/18.
+//  Copyright © 2018 Dũng Võ. All rights reserved.
+//
+
+import Foundation
+
+struct ToDo {
+    var title : String
+    var isComplete : Bool
+    var dueDate : Date
+    var notes : String?
+    
+    static func loadToDos() -> [ToDo]? {
+        return nil
+    }
+    
+    static func loadSampleToDos() -> [ToDo] {
+        let toDo1 = ToDo(title: "ToDo One", isComplete: false, dueDate: Date(), notes: "Notes 1")
+        let toDo2 = ToDo(title: "ToDo Two", isComplete: false, dueDate: Date(), notes: "Notes 2")
+        let toDo3 = ToDo(title: "ToDo Three", isComplete: false, dueDate: Date(), notes: "Notes 3")
+        
+        return [toDo1,toDo2,toDo3]
+    }
+}
+
---
+++ ToDoList/View Controllers/ToDoTableViewController.swift
@@ -0,0 +1,98 @@
+//
+//  ToDoTableViewController.swift
+//  ToDoList
+//
+//  Created by Dũng Võ on 2/28/18.
+//  Copyright © 2018 Dũng Võ. All rights reserved.
+//
+
+import UIKit
+
+class ToDoTableViewController: UITableViewController {
+
+    var toDos = [ToDo]()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+       navigationController?.navigationBar.prefersLargeTitles = true
+        
+        if let saveToDos = ToDo.loadToDos() {
+            toDos = saveToDos
+        }else {
+            toDos = ToDo.loadSampleToDos()
+        }
+
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return toDos.count
+    }
+
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoCell")  else {
+            fatalError("Could not dequeue a cell")
+        }
+        
+        let toDo = toDos[indexPath.row]
+        cell.textLabel?.text = toDo.title
+        
+        return cell
+    }
+    
+
+    /*
+    // Override to support conditional editing of the table view.
+    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the specified item to be editable.
+        return true
+    }
+    */
+
+    /*
+    // Override to support editing the table view.
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            // Delete the row from the data source
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        } else if editingStyle == .insert {
+            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+        }    
+    }
+    */
+
+    /*
+    // Override to support rearranging the table view.
+    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
+
+    }
+    */
+
+    /*
+    // Override to support conditional rearranging of the table view.
+    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the item to be re-orderable.
+        return true
+    }
+    */
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
