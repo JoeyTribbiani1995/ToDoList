8ee50d14569e05c8ca749463ecbe5d508baf0fbf
JoeyTribbiani <vophanhongdung@gmail.com>
2018-02-28 14:05:28 +0700

    commit disable save bar button item

--- ToDoList/View Controllers/ToDoDetailTableViewController.swift
+++ ToDoList/View Controllers/ToDoDetailTableViewController.swift
@@ -10,15 +10,39 @@ import UIKit
 
 class ToDoDetailTableViewController: UITableViewController {
 
+    @IBOutlet weak var isCompleteButton: UIButton!
+    @IBOutlet weak var titleTextField: UITextField!
+    @IBOutlet weak var đueDateLabel: UILabel!
+    @IBOutlet weak var dueDatePicker: UIDatePicker!
+    @IBOutlet weak var notesTextView: UITextView!
+    @IBOutlet weak var saveBarButton: UIBarButtonItem!
+    
+    
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        updateSaveButton()
         
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
     }
+    @IBAction func datePIckerValueChanged(_ sender: UIDatePicker) {
+    }
+    
+    @IBAction func textEditingChanged(_ sender: UITextField) {
+        updateSaveButton()
+    }
+    func updateSaveButton(){
+        let title = titleTextField.text ?? ""
+        let notes = notesTextView.text ?? ""
+        
+        saveBarButton.isEnabled = !title.isEmpty && !notes.isEmpty
+    }
+    
+    
  
    
 
