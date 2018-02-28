33cb4ce1b7437b98107f4d5c1eaa5526f4b19964
JoeyTribbiani <vophanhongdung@gmail.com>
2018-02-28 14:54:16 +0700

    commit update date time in label

--- ToDoList/View Controllers/ToDoDetailTableViewController.swift
+++ ToDoList/View Controllers/ToDoDetailTableViewController.swift
@@ -12,7 +12,7 @@ class ToDoDetailTableViewController: UITableViewController {
 
     @IBOutlet weak var isCompleteButton: UIButton!
     @IBOutlet weak var titleTextField: UITextField!
-    @IBOutlet weak var đueDateLabel: UILabel!
+    @IBOutlet weak var dueDateLabel: UILabel!
     @IBOutlet weak var dueDatePicker: UIDatePicker!
     @IBOutlet weak var notesTextView: UITextView!
     @IBOutlet weak var saveBarButton: UIBarButtonItem!
@@ -24,23 +24,38 @@ class ToDoDetailTableViewController: UITableViewController {
         super.viewDidLoad()
         updateSaveButton()
         
+        dueDatePicker.date = Date().addingTimeInterval(24*60*60)
+        updateDueDateLabel(date: dueDatePicker.date)
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
     }
+    
     @IBAction func datePIckerValueChanged(_ sender: UIDatePicker) {
+        updateDueDateLabel(date: dueDatePicker.date)
     }
     
     @IBAction func textEditingChanged(_ sender: UITextField) {
         updateSaveButton()
     }
+    
     func updateSaveButton(){
         let title = titleTextField.text ?? ""
         let notes = notesTextView.text ?? ""
         
         saveBarButton.isEnabled = !title.isEmpty && !notes.isEmpty
     }
+    
+    @IBAction func returnPressed(_ sender: UITextField) {
+        titleTextField.resignFirstResponder()
+    }
+    
+    func updateDueDateLabel(date : Date){
+        dueDateLabel.text = ToDo.dueDateFormatter.string(from: date)
+    }
+    
     
     
  
