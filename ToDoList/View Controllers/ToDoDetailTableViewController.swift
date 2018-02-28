948b6a216a1fd73ac73d5ecfe3400db6661f1bcf
JoeyTribbiani <vophanhongdung@gmail.com>
2018-02-28 13:46:11 +0700

    commit create static table view

---
+++ ToDoList/View Controllers/ToDoDetailTableViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ToDoDetailTableViewController.swift
+//  ToDoList
+//
+//  Created by Dũng Võ on 2/28/18.
+//  Copyright © 2018 Dũng Võ. All rights reserved.
+//
+
+import UIKit
+
+class ToDoDetailTableViewController: UITableViewController {
+
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+ 
+   
+
+}
